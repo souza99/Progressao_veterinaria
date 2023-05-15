09c559ae8c44bd158902ad86d03312f0d3b175b9
souza99 <jpfsouza99@gmail.com>
2023-05-14 21:18:44 -0300

    criando redirecionamento

--- cadastros/urls.py
+++ cadastros/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 
+from .views import FazendaCreate, AnimalCreate, QualidadeLeiteCreate
 
 urlpatterns = [
-    
+    path('cadastrar/fazenda/', FazendaCreate.as_view(), name='fazenda_create'),
+    path('cadastrar/animal/', AnimalCreate.as_view(), name='animal_create'),
+    path('cadastrar/qualidadeleite/', QualidadeLeiteCreate.as_view(), name='qualidadeleite_create'),       
 ]
--- cadastros/views.py
+++ cadastros/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from django.views.generic.edit import CreateView
 
+from .models import Fazenda, Animal, QualidadeLeite
+
+from django.urls import reverse_lazy
 # Create your views here.
+class FazendaCreate(CreateView):
+    model = Fazenda
+    fields = ['nome', 'localizacao', 'cpfcnpj', 'numero']
+    template_name = 'cadastros/form.html'
+    success_url = reverse_lazy('index')
+    
+class AnimalCreate(CreateView):
+    model = Animal
+    fields = ['nome', 'lote', 'nomePai', 'numeroPai', 'nomeMae', 'numeroMae', 'motivoBaixa', 'partosNaoLancados', 'dataNascimento', 'dataBaixa', 'fazenda']
+    template_name = 'cadastros/form.html'
+    success_url = reverse_lazy('index')
+    
+class QualidadeLeiteCreate(CreateView):
+    model = QualidadeLeite
+    fields = ['nt', 'dias', 'estado', 'kgLeite', 'porcentagemGordura', 'porcentagemProteinas', 'lactose', 'gp', 'ccs', 'ureia', 'cetose', 'pps', 'animal', 'diaRetirado']
+    template_name = 'cadastros/form.html'
+    success_url = reverse_lazy('index')
+    
